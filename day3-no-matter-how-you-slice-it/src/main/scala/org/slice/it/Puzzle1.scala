6533418071d5b25d48bd31b62de1870d89f541bb
Marco Wittig <marco.wittig@jambit.com>
2018-12-03 18:53:54 +0100

    Day 3: Comment for another solution for puzzle 1

--- day3-no-matter-how-you-slice-it/src/main/scala/org/slice/it/Puzzle1.scala
+++ day3-no-matter-how-you-slice-it/src/main/scala/org/slice/it/Puzzle1.scala
@@ -13,4 +13,14 @@ object Puzzle1 extends App {
       }
   }
   println(area)
+
+
+  // This doesn't work, perhaps a better idea wold be to
+  // * create a giant data structure (2d- array) with 0
+  // * Take a claim
+  // * check the space for the claim
+  // * if it is 0 put in the claim and store a 1 into the array
+  // * if it is 1 put in the claim and store a x into the array
+  // count the x
+
 }
